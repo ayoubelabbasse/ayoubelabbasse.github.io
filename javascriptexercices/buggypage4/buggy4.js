18dfc491a06dc08005eafe734ecb142c32ad7fd6
ayoubelabbasse <belabsayoub@gmail.com>
2021-03-08 05:23:34 -0600

    compr

--- javascriptexercices/buggypage4/buggy4.js
+++ javascriptexercices/buggypage4/buggy4.js
@@ -1,6 +1,6 @@
 function somethingElse() {
     if (5 > 2) {
-        alert("5 is Bigger than");
+        alert("5 is Bigger than 2");
     }else if (5 < 2) {
             alert("5 is Smaller than 2");
         }
